2cf0f723dbb6840456256ff5a62f20a33ed9280f
n-lacoste <nicklacoste44@gmail.com>
2026-08-27 22:35:58 -0400

    Update nav.js

--- nav.js
+++ nav.js
@@ -17,6 +17,8 @@
           <a class="nav-menu-title" href="${base}music/">Music</a>
           <div class="nav-dropdown">
             <a href="${base}music/">Home: Music</a>
+            <a href="${base}albums/">Top Albums</a>
+            <a href="${base}artists/">Top Artists</a>
           </div>
         </div>
 
